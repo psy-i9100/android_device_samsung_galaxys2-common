e079c26f60a3be283c037eaec419663b6cca5eec
psyafter <psyafter@gmail.com>
2015-08-12 15:34:49 +0300

    Update graphics.c

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -210,7 +210,7 @@ int gr_text(int x, int y, const char *s, ...)
     return gr_text_impl(x, y, s, 0);
 }
 
-int gr_text(int x, int y, const char *s, int bold)
+int gr_text_impl(int x, int y, const char *s, int bold)
 {
     GGLContext *gl = gr_context;
     GRFont *font = gr_font;
